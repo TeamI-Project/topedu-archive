dc2c12a0228b151830d7e37df17717e7e953d8ef
jujuwon <webmon@kakao.com>
2022-03-12 15:14:01 +0900

    fix: CORS in node

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => res.send("Hello World~!"));
 
-app.get("/api/newModify", (req, res) => {
+app.get("/api/newModify", cors(), (req, res) => {
     res.status(200).json({
         firstLevel : {
             regEnglish : "2022-03-12",
