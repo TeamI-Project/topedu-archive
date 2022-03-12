92e8f4e236505d6bcba081d3d120d70db3512786
jujuwon <webmon@kakao.com>
2022-03-12 15:55:30 +0900

    test: res header

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,19 @@ app.get("/", (req, res) => res.send("Hello World~!"));
 
 app.get("/api/newModify", cors(), (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.status(200).json({
-        firstLevel : {
-            regEnglish : "2022-03-12",
-            lvEnglish : "lv1",
-            regMath : "2022-03-12",
-            lvMath : "lv2"
-        }
-    });
+    try {
+        res.status(200).json({
+            firstLevel : {
+                regEnglish : "2022-03-12",
+                lvEnglish : "lv1",
+                regMath : "2022-03-12",
+                lvMath : "lv2"
+            }
+        });
+    } catch (err) {
+        res.status(500);
+        res.send(err.message);
+    }
 })
 
 app.post("/api/newModify", (req, res) => {
