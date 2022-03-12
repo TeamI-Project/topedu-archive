b9987da5d03fda5712636a693cf39f4a6eeec69c
jujuwon <webmon@kakao.com>
2022-03-12 15:58:08 +0900

    test: change url

--- frontend/assets/js/newModify.js
+++ frontend/assets/js/newModify.js
@@ -3,7 +3,7 @@ var regLvEng = document.getElementById("regLvEng");
 var regAtMath = document.getElementById("regAtMath");
 var regLvMath = document.getElementById("regLvMath");
 
-const url = "https://localhost:8000/api/newModify"
+const url = "http://top-edu.co.kr:8000/api/newModify"
 
 fetch(url).then(function(res){
     res.json().then(function(json){
