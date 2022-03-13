5cbb6cfd2dc9abf5acf146773c7b6d24cf83b51a
sladuf <04fud27@gmail.com>
2022-03-13 13:27:12 +0900

    fix : db structure

--- db/sql/create_database.sql
+++ db/sql/create_database.sql
@@ -1,40 +1,96 @@
 DROP DATABASE IF EXISTS archive;
 CREATE DATABASE archive DEFAULT CHARSET=utf8;
 
+/*
+modifyer : 수정한 사람
+modifyDate : 수정한 날짜
+*/
+
 USE archive;
+/*
+branch : 지점 (MA/CH/SA)
+nowStatus : 퇴원여부 (0/1)
+*/
 DROP TABLE IF EXISTS Students;
 CREATE TABLE Students (
     studentID CHAR(20),
     studentName CHAR(20) NOT NULL,
-    levelEng TINYINT,
-    levelMath TINYINT,
-    consultation TEXT,
-    checklist VARCHAR(255),
-    registerDate TIMESTAMP DEFAULT now(),
+    studentImg VARCHAR(255),
+    branch VARCHAR(10),
+    nowStatus TINYINT,
     PRIMARY KEY (studentID)
 );
 
-DROP TABLE IF EXISTS Consulting;
-CREATE TABLE Consulting (
+DROP TABLE IF EXISTS NewRecord;
+CREATE TABLE NewRecord (
     studentID CHAR(20),
-    consultingType TINYINT NOT NULL,
-    content TEXT,
-    consultingDate TIMESTAMP DEFAULT now(),
-    FOREIGN KEY (studentID) REFERENCES Students (studentID)
+    regEng DATE,
+    levelEng TINYINT,
+    regMath DATE,
+    levelMath TINYINT,
+    testEng VARCHAR(255),
+    testMath VARCHAR(255),
+    friendShip TINYINT,
+    personality TINYINT,
+    parentShip TINYINT,
+    concentration TINYINT,
+    homework TINYINT,
+    comment TEXT,
+    checklist VARCHAR(255),
+    modifyer VARCHAR(20),
+    modifyDate TIMESTAMP DEFAULT now(),
+    PRIMARY KEY (studentID)
 );
 
+/*
+careerNet : pdf_link
+*/
 DROP TABLE IF EXISTS Testpaper;
 CREATE TABLE Testpaper (
     studentID CHAR(20),
-    testData VARCHAR(255) NOT NULL,
+    SCA VARCHAR(255),
+    CPS VARCHAR(255),
+    careerNet VARCHAR(255),
+    sixSence VARCHAR(255),
+    testEtc VARCHAR(255),
+    modifyer VARCHAR(20),
+    modifyDate TIMESTAMP DEFAULT now(),
     FOREIGN KEY (studentID) REFERENCES Students (studentID)
 );
 
 DROP TABLE IF EXISTS Grade;
 CREATE TABLE Grade (
     studentID CHAR(20),
-    examType TINYINT NOT NULL,
-    reportCard VARCHAR(255),
-    gradeDate TIMESTAMP DEFAULT now(),
+    midType TINYINT NOT NULL,
+    midCard VARCHAR(255),
+    highType TINYINT NOT NULL,
+    highCard VARCHAR(255),
+    modifyer VARCHAR(20),
+    modifyDate TIMESTAMP DEFAULT now(),
     FOREIGN KEY (studentID) REFERENCES Students (studentID)
 );
+
+DROP TABLE IF EXISTS Monthly;
+CREATE TABLE Monthly (
+    studentID CHAR(20),
+    monthType TINYINT NOT NULL,
+    monthCard VARCHAR(255),
+    modifyer VARCHAR(20),
+    modifyDate TIMESTAMP DEFAULT now(),
+    FOREIGN KEY (studentID) REFERENCES Students (studentID)
+);
+
+DROP TABLE IF EXISTS Consulting;
+CREATE TABLE Consulting (
+    studentID CHAR(20),
+    teacherComment TEXT,
+    studentComment TEXT,
+    parentsComment TEXT,
+    modifyDate TIMESTAMP DEFAULT now(),
+    modifyer VARCHAR(20),
+    modifyDate TIMESTAMP DEFAULT now(),
+    FOREIGN KEY (studentID) REFERENCES Students (studentID)
+);
+
+
+
