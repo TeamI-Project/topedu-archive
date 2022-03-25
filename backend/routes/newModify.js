d265a595f91c4130d31672824060aeeb6dba80d6
jujuwon <webmon@kakao.com>
2022-03-25 20:00:41 +0900

    feat: mysql connection

--- backend/routes/newModify.js
+++ backend/routes/newModify.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+const dbconfig = require('../config/mysql.js');
+const connection = mysql.createConnection(dbconfig);
 // const cors = require('cors');
+// ALTER USER 'topedu'@'localhost' IDENTIFIED WITH mysql_native_password BY 'topedu';
 
 router.get("/", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    try {
-        res.status(200).json({
-            firstLevel : {
-                regEnglish : "2022-03-12",
-                lvEnglish : "lv1",
-                regMath : "2022-03-12",
-                lvMath : "lv2"
-            }
-        });
-    } catch (err) {
-        res.status(500);
-        res.send(err.message);
-    }
+    connection.query('SELECT * from Students', (err, rows) => {
+        if (err) throw err;
+        res.header("Access-Control-Allow-Origin", "*");
+        try {
+            res.status(200).json({
+                rows
+                // firstLevel : {
+                //     regEnglish : "2022-03-12",
+                //     lvEnglish : "lv1",
+                //     regMath : "2022-03-12",
+                //     lvMath : "lv2"
+                // },
+                // levelTest : {
+                //     english : "test",
+                //     math : "test"
+                // },
+                // newConsulting : {
+                //     friendShip : 1,
+                //     personality : 2,
+                //     parentShip : 3,
+                //     concentration : 4,
+                //     homework : 5,
+                //     textbox : "test"
+                // },
+                // newCheckList : {
+                //     checkList : "test"
+                // }
+            });
+        } catch (err) {
+            res.status(500);
+            res.send(err.message);
+        }         
+    });
 })
 
 module.exports = router;
