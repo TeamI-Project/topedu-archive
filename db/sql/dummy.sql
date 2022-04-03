df3ee79cb8fa6f81881d8a8838542612618b0c08
jujuwon <webmon@kakao.com>
2022-04-03 17:00:36 +0900

    fix: dummy query

--- db/sql/dummy.sql
+++ db/sql/dummy.sql
@@ -1,3 +1,5 @@
+USE archive;
+
 INSERT INTO Students VALUES("mtop1234", "원령이", "ex_face.jpg", "MA", 1);
 
 INSERT INTO NewRecord (studentID, regEng, levelEng, testEng, friendShip, personality, parentShip, concentration, homework, checklist, modifyer) VALUES("mtop1234", "22-02-01", "SproutA", "dummy_img.jpeg", 0,1,0,2,1, "dummy_img.jpeg", "teacher727");
