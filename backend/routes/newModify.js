ad3777aff0589c392afcfa52ba4c67c20dd51e18
jujuwon <webmon@kakao.com>
2022-04-04 00:11:04 +0900

    feat: newModify

--- backend/routes/newModify.js
+++ backend/routes/newModify.js
@@ -6,32 +6,35 @@ const connection = mysql.createConnection(dbconfig);
 // const cors = require('cors');
 // ALTER USER 'topedu'@'localhost' IDENTIFIED WITH mysql_native_password BY 'topedu';
 
+// Table NewRecord
 router.get("/", (req, res) => {
-    connection.query('SELECT * from Students', (err, rows) => {
+    const id = req.query.id;
+    const query = "SELECT * from NewRecord WHERE studentID='" + id + "'";
+    connection.query(query, (err, rows) => {
         if (err) throw err;
         res.header("Access-Control-Allow-Origin", "*");
         try {
             res.status(200).json({
                 firstLevel : {
-                    regEnglish : "2022-03-12",
-                    lvEnglish : "lv1",
-                    regMath : "2022-03-12",
-                    lvMath : "lv2"
+                    regEnglish : rows[0].regEng,
+                    lvEnglish : rows[0].levelEng,
+                    regMath : rows[0].regMath,
+                    lvMath : rows[0].levelMath
                 },
                 levelTest : {
-                    english : "test",
-                    math : "test"
+                    english : [rows[0].testEng],
+                    math : [rows[0].testMath]
                 },
                 newConsulting : {
-                    friendShip : 1,
-                    personality : 2,
-                    parentShip : 3,
-                    concentration : 4,
-                    homework : 5,
-                    textbox : "test"
+                    friendShip : rows[0].friendShip,
+                    personality : rows[0].personality,
+                    parentShip : rows[0].parentShip,
+                    concentration : rows[0].concentration,
+                    homework : rows[0].homework,
+                    textbox : rows[0].comment
                 },
                 newCheckList : {
-                    checkList : "test"
+                    checkList : rows[0].checklist
                 }
             });
         } catch (err) {
@@ -40,6 +43,10 @@ router.get("/", (req, res) => {
             res.send(err.message);
         }         
     });
+})
+
+router.post("/", (req, res) => {
+    res.send("success newModify send");
 })
 
 module.exports = router;
