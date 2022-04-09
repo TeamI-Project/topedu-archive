35734469cec12a147075792b4cff8a473bee03be
jujuwon <webmon@kakao.com>
2022-04-09 20:40:29 +0900

    fix: bodyparser

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const privateKey = fs.readFileSync("/etc/letsencrypt/live/top-edu.co.kr/privkey.pem", "utf8");
 const certificate = fs.readFileSync("/etc/letsencrypt/live/top-edu.co.kr/cert.pem", "utf8")
 const ca = fs.readFileSync("/etc/letsencrypt/live/top-edu.co.kr/chain.pem", "utf8")
@@ -16,8 +19,6 @@ const credentials = {
     ca: ca
 };
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cors());
 
 const newModify = require('./routes/newModify');
