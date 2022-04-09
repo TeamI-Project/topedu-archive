90e93dd4506c682770bafc648ba4420a01384584
sladuf <04fud27@gmail.com>
2022-04-09 15:43:20 +0900

    fix: js file

--- frontend/assets/js/commentModify.js
+++ frontend/assets/js/commentModify.js
@@ -3,14 +3,28 @@ let student = document.getElementById("studentComment");
 let parents = document.getElementById("parentsComment");
 let etc = document.getElementById("etcComment");
 
-const url = "http://top-edu.co.kr:8000/api/gradeModify";
+const url = "http://top-edu.co.kr:8000/api/commentModify";
 
 fetch(url).then(function(res){
     res.json().then(function(json){
-        teacher.innerHTML += '<textarea id="teacherText" cols="30" rows="10"></textarea>';
-        student.innerHTML += '<textarea id="studentText" cols="30" rows="10"></textarea>';
-        parents.innerHTML += '<textarea id="parentsText" cols="30" rows="10"></textarea>';
-        etc.innerHTML += '<textarea id="etcText" cols="30" rows="10"></textarea>';
+        teacher.innerHTML += '<textarea id="teacherText" cols="30" rows="10">'+json.teacherComment+'</textarea>';
+        student.innerHTML += '<textarea id="studentText" cols="30" rows="10">'+studentComment+'</textarea>';
+        parents.innerHTML += '<textarea id="parentsText" cols="30" rows="10">'+parentsComment+'</textarea>';
+        etc.innerHTML += '<textarea id="etcText" cols="30" rows="10"'+etcComment+'></textarea>';
         
     })
 })
+
+function doneModify(){
+    teacher = document.getElementById('teacherText').value;
+    student = document.getElementById('studentText').value;
+    parents = document.getElementById('parentsText').value;
+    etc = document.getElementById('etcText').value;
+    
+    var data = {
+        teacher : teacher,
+        student : student,
+        parents : parents,
+        etc : etc
+    }
+}
