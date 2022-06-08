0ac5934a011d3425f8662e4182d2b5da38758212
sladuf <04fud27@gmail.com>
2022-06-08 23:22:30 +0900

    fix : register branch

--- frontend/assets/js/register.js
+++ frontend/assets/js/register.js
@@ -1,26 +1,26 @@
-// function teacherCookie() {
-//     var result = null;
-//     var cookie = document.cookie.split(';');
-//     cookie.some(function (item) {
-//         // 공백을 제거
-//         item = item.replace(' ', '');
+function teacherCookie() {
+    var result = null;
+    var cookie = document.cookie.split(';');
+    cookie.some(function (item) {
+        // 공백을 제거
+        item = item.replace(' ', '');
  
-//         var dic = item.split('=');
+        var dic = item.split('=');
 
-//         var key = "teacherID";
+        var key = "teacherID";
 
-//         if (key === dic[0]) {
-//             result = dic[1];
-//             return true;    // break;
-//         }
-//     });
-//     return result;
-// }
-// var teacherID = teacherCookie();
-// if (teacherID == null){
-//     alert("로그인 하세요.")
-//     location.href = "loginPage.html"
-// }
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;    // break;
+        }
+    });
+    return result;
+}
+var teacherID = teacherCookie();
+if (teacherID == null){
+    alert("로그인 하세요.")
+    location.href = "loginPage.html"
+}
 
 const getBranch = "https://archive.top-edu.co.kr:8000/api/teacher?id="+teacherID;
 var branch = "";
